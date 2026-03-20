09b95e5faae0ceb8eadf83e8b5fa2521843fa765
pritam goswami <mousummi92@gmail.com>
2026-03-20 13:10:43 +0530

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,10 @@
+movies=[]
+movie1=input("Entre a movie name:- ")
+movie2=input("Entre a movie name:- ")
+movie3=input("Entre a movie name:- ")
+movies.append(movie2)
+movies.append(movie3)
+movies.append(movie1)
+movies.sort()
+print(f"So your entered movies are:",movies)
+#this is a list for movies 
